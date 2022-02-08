5a639ae3de3308ed11a0ef5a77abda02f4c2f57d
jvaquero <jvaquero@aidacanarias.com>
2022-02-08 07:11:13 +0000

    Red - 4:true

--- LeapYearKata.Tests/YearShould.cs
+++ LeapYearKata.Tests/YearShould.cs
@@ -38,13 +38,13 @@ namespace LeapYearKata.Tests {
                 ///   Year that is divisible by 400 IS a leap year
                 yield return new TestFixtureData(400, true);
                 yield return new TestFixtureData(800, true);
-
                 yield return new TestFixtureData(401, false);
 
                 ///   Year that is divisible by 100 but not by 400 IS NOT a leap year
                 yield return new TestFixtureData(100, false);
 
                 ///   Year that is divisible by 4 but not by 100 IS a leap year
+                yield return new TestFixtureData(4, true);
 
 
                 ///   Year that is not divisible by 4 IS NOT a leap year
