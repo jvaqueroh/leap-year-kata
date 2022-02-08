1b089cd1c0638158f3b0ad276fdbd3a9fc5b5999
jvaquero <jvaquero@aidacanarias.com>
2022-02-08 07:19:10 +0000

    Green - 4:true

--- LeapYearKata/Year.cs
+++ LeapYearKata/Year.cs
@@ -11,8 +11,19 @@ namespace LeapYearKata {
 
         public bool IsLeapYear()
         {
-            var module = value % 400;
-            return module == 0;
+            var isDivisibleBy400 = (value % 400) == 0;
+            if (isDivisibleBy400) 
+                return true;
+            
+            var isDivisibleBy100 = (value % 100) == 0;
+            if(isDivisibleBy100 && !isDivisibleBy400)
+                return false;
+
+            var isDivisibleBy4 = (value % 4) == 0;
+            if (isDivisibleBy4 && !isDivisibleBy100)
+                return true;
+            
+            return false;
         }
     }
 }
